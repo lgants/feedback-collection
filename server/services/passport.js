8a8bc791fee3fc378dd8979714c6049a8ae2fa16
lgants <lgants@gmail.com>
2017-08-27 12:06:09 -0500

    added acync/await syntax

--- server/services/passport.js
+++ server/services/passport.js
@@ -24,16 +24,15 @@ passport.use(
     callbackURL: '/auth/google/callback',
     proxy: true,
   },
-    (accessToken, refreshToken, profile, done) => {
-      User.findOne({ googleId: profile.id })
-        .then((user) => {
-          if (user) {
-            done(null, user);
-          } else {
-            new User({ googleId: profile.id }).save()
-              .then(user => done(null, user));
-          }
-        });
+    async (accessToken, refreshToken, profile, done) => {
+      const existingUser = await User.findOne({ googleId: profile.id });
+
+      if (existingUser) {
+        return done(null, existingUser);
+      }
+
+      const newUser = await new User({ googleId: profile.id }).save();
+      done(null, newUser);
     }
   )
 );
